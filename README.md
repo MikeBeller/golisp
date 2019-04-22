8a8aefdc79a9288cf2383eef582b11ca05ca7976
mike <mike@hal.lan>
2019-04-22 18:08:03 -0400

    Add benchmarks to show that list1 == list2 in go actually checks the whole list!

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # golisp
 
 Building a simple LISP in go, just for fun.
+
+Interesting thing learned: Recursive go interfaces have a
+recursive notion of equality.   This means that in my
+implementation of LISP, list1 == list2 checks the full 
+list for equality, not just the first element!
+
+See equal_bench_test.go
+
+mike@hal:~/github/golisp$ go test -bench=List
+goos: linux
+goarch: amd64
+BenchmarkList100-4           	 1000000	      1235 ns/op
+BenchmarkList1k-4            	  100000	     13268 ns/op
+BenchmarkList10k-4           	   10000	    141987 ns/op
+BenchmarkList10kDiff-4       	   10000	    143047 ns/op
+BenchmarkList10kDiffSoon-4   	10000000	       128 ns/op
+PASS
+
+You can see that for a list of 10k it takes 10 times as long to
+do == as a list of 1k, which takes 10 times as long as a list of 100.
+
+Also notice that if the two lists differ early on, the comparison
+is fast (List10kDiffSoon)
+
---
+++ equal_bench_test.go
@@ -0,0 +1,87 @@
+package golisp
+
+import (
+	"fmt"
+	"testing"
+)
+
+var _ = fmt.Println
+
+func BenchmarkList100(b *testing.B) {
+	v := Symbol("A")
+	x := Pair{v, nil}
+	y := Pair{v, nil}
+	for i := 0; i < 100; i++ {
+		x = Pair{v, x}
+		y = Pair{v, y}
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = x == y
+	}
+}
+
+func BenchmarkList1k(b *testing.B) {
+	v := Symbol("A")
+	x := Pair{v, nil}
+	y := Pair{v, nil}
+	for i := 0; i < 1000; i++ {
+		x = Pair{v, x}
+		y = Pair{v, y}
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = x == y
+	}
+}
+
+func BenchmarkList10k(b *testing.B) {
+	v := Symbol("A")
+	x := Pair{v, nil}
+	y := Pair{v, nil}
+	for i := 0; i < 10000; i++ {
+		x = Pair{v, x}
+		y = Pair{v, y}
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = x == y
+	}
+}
+
+func BenchmarkList10kDiff(b *testing.B) {
+	v := Symbol("A")
+	x := Pair{v, nil}
+	y := Pair{v, v}
+	for i := 0; i < 10000; i++ {
+		x = Pair{v, x}
+		y = Pair{v, y}
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = x == y
+	}
+}
+
+/* This shows that if the two interface chains differ early in
+ * the chain, the comparison is very fast.  Vs. if the two interface
+ * chains differ at the end, the comparison is slow.*/
+
+func BenchmarkList10kDiffSoon(b *testing.B) {
+	v := Symbol("A")
+	x := Pair{v, nil}
+	y := Pair{v, nil}
+	for i := 0; i < 10000; i++ {
+		if i == 9990 {
+			x = Pair{v, x}
+			y = Pair{Symbol("B"), y}
+		} else {
+			x = Pair{v, x}
+			y = Pair{v, y}
+		}
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = x == y
+	}
+}
