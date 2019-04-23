4074b52693114cd5b21459c05b82cb09d9873e04
mike <mike@hal.lan>
2019-04-23 18:04:14 -0400

    Add a test for Cdr to see if function calls copy the whole list.

--- equal_bench_test.go
+++ equal_bench_test.go
@@ -85,3 +85,30 @@ func BenchmarkList10kDiffSoon(b *testing.B) {
 		_ = x == y
 	}
 }
+
+// These two tests show that, unlike ==, calling a function
+// on these long chains of interfaces doesn't seem to incur
+// a copy (even though with == they seem to have value semantics).
+func BenchmarkCdr1k(b *testing.B) {
+	v := Symbol("A")
+	x := Pair{v, nil}
+	for i := 0; i < 1000; i++ {
+		x = Pair{v, x}
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = cdr(x)
+	}
+}
+
+func BenchmarkCdr10k(b *testing.B) {
+	v := Symbol("A")
+	x := Pair{v, nil}
+	for i := 0; i < 10000; i++ {
+		x = Pair{v, x}
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = cdr(x)
+	}
+}
