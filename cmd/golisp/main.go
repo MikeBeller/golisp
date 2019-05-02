a83c8d575dfb289b9a03e7ca2b8dc13f1cc82d85
mike <mike@hal.lan>
2019-05-01 20:49:05 -0400

    More main stuff

--- cmd/golisp/main.go
+++ cmd/golisp/main.go
@@ -6,10 +6,13 @@ import (
 	"github.com/mikebeller/golisp/lisp"
 	"io/ioutil"
 	"log"
+	"os"
 )
 
 func main() {
 	dbgPtr := flag.Bool("debug", false, "Enable debug printing")
+	envPtr := flag.String("env", "'()", "Environment")
+	envFilePtr := flag.String("envfile", "", "Environment file")
 	flag.Parse()
 
 	if *dbgPtr {
@@ -17,15 +20,15 @@ func main() {
 	}
 
 	if len(flag.Args()) < 1 {
-		log.Fatal("Usage: golisp [--debug] expr [envfile]")
+		fmt.Fprintf(os.Stderr, "Usage: %s [flags] expr\n", os.Args[0])
+		flag.PrintDefaults()
+		log.Fatal("exiting")
 	}
 	progStr := flag.Args()[0]
 
-	envStr := "()"
-
-	if len(flag.Args()) > 1 {
-		envFile := flag.Args()[1]
-		envBytes, err := ioutil.ReadFile(envFile)
+	envStr := *envPtr
+	if *envFilePtr != "" {
+		envBytes, err := ioutil.ReadFile(*envFilePtr)
 		if err != nil {
 			log.Fatal(err)
 		}
